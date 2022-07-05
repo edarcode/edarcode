3fa0a650fd838c4b1724be18caa1c336352132a6
Edwin Ortiz <edarcode@gmail.com>
2022-07-05 15:29:26 -0500

    feat(*) estructurando componentes para el header

--- src/components/container/Header/Header.jsx
+++ src/components/container/Header/Header.jsx
@@ -1,10 +1,16 @@
-import Nav from "../Nav/Nav";
+import Banner from "../../common/Banner/Banner";
+import BlogNav from "../BlogNav/BlogNav";
+import MainNav from "../MainNav/MainNav";
+import SocialNav from "../SocialNav/SocialNav";
 import css from "./style.module.css";
 
 export default function Header() {
 	return (
 		<header className={css.header}>
-			<Nav />
+			<MainNav />
+			<SocialNav />
+			<BlogNav />
+			<Banner />
 		</header>
 	);
 }
