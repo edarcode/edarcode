41bebfa802b4cf9e4f2989c4469b3e4b87ca0611
Edwin Ortiz <edarcode@gmail.com>
2022-07-06 12:02:33 -0500

    feat(src/components/common/Banner/Banner.jsx)

--- src/components/common/Banner/Banner.jsx
+++ src/components/common/Banner/Banner.jsx
@@ -1,3 +1,21 @@
+import css from "./style.module.css";
+
 export default function Banner() {
-	return <div></div>;
+	return (
+		<div className={css.banner}>
+			<img className={css.banner__img} src="/edarko.png" alt="edarcode" />
+			<div className={css.banner__name}>
+				{renderSpanByChar("edarcode", css.banner__char)}
+			</div>
+		</div>
+	);
+}
+
+function renderSpanByChar(text, className) {
+	const textArr = text.split("");
+	return textArr.map((item, index) => (
+		<span className={className} key={index}>
+			{item}
+		</span>
+	));
 }
