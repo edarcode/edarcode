64f5a2b392f371a8e279746cd168a483b2871028
Edwin Ortiz <edarcode@gmail.com>
2022-07-06 10:18:16 -0500

    feat(src/components/container/MainNav/MainNav.jsx)

--- src/components/container/MainNav/MainNav.jsx
+++ src/components/container/MainNav/MainNav.jsx
@@ -1,3 +1,29 @@
+import { CSS, HTML, javascript, terminal } from "../../../constants/path";
+import BoxLink from "../../common/BoxLink/BoxLink";
+import css from "./style.module.css";
+
 export default function MainNav() {
-	return <nav></nav>;
+	return (
+		<nav className={css.mainnav}>
+			<BoxLink
+				path={HTML}
+				text="HTML"
+				img={"/html.svg"}
+				color="hsl(25, 100%, 50%)"
+			/>
+			<BoxLink path={CSS} text="CSS" img={"/css.svg"} color="royalblue" />
+			<BoxLink
+				path={javascript}
+				text="javascript"
+				img={"/js.svg"}
+				color="hsl(60, 100%, 30%)"
+			/>
+			<BoxLink
+				path={terminal}
+				text="terminal"
+				img={"/terminal.svg"}
+				color="hsl(0, 0%, 7%)"
+			/>
+		</nav>
+	);
 }
--- src/constants/path.js
+++ src/constants/path.js
@@ -1,1 +1,5 @@
 export const home = "/";
+export const HTML = "/html";
+export const CSS = "/css";
+export const javascript = "/javascript";
+export const terminal = "/terminal";
