b9159a0fa16fb1ccfb33f1b7fec60ce15447ae79
Edwin Ortiz <edarcode@gmail.com>
2022-07-06 01:52:55 -0500

    feat(src/components/container/SocialNav/SocialNav.jsx) done

--- src/components/common/BoxLink/BoxLink.jsx
+++ src/components/common/BoxLink/BoxLink.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import css from "./style.module.css";
 
-export default function BoxLink({ text, img, color, path }) {
+export default function BoxLink({ text, img, color, path, a }) {
+	const link = useRef(null);
+	useEffect(() => {
+		if (color) link.current.style.color = color;
+	}, [color]);
 	if (!path) return null;
+	if (a)
+		return (
+			<a
+				href={path}
+				target="_blank"
+				rel="noreferrer"
+				className={css.box}
+				ref={link}
+			>
+				{img && <img className={css.box__img} src={img} alt={img} />}
+				{text && <span className={css.box__text}>{text}</span>}
+			</a>
+		);
 	return (
-		<Link to={path} className={css.box}>
+		<Link to={path} className={css.box} reloadDocument ref={link}>
 			{img && <img className={css.box__img} src={img} alt={img} />}
-			<span className={css.box__text}>{text}</span>
+			{text && <span className={css.box__text}>{text}</span>}
 		</Link>
 	);
 }
--- src/components/container/SocialNav/SocialNav.jsx
+++ src/components/container/SocialNav/SocialNav.jsx
@@ -1,3 +1,21 @@
+import {
+	discord,
+	github,
+	instagram,
+	linkedin,
+	youtube
+} from "../../../constants/urls";
+import BoxLink from "../../common/BoxLink/BoxLink";
+import css from "./style.module.css";
+
 export default function SocialNav() {
-	return <div></div>;
+	return (
+		<nav className={css.social}>
+			<BoxLink a path={github} img="/github.svg" color="hsl(0, 0%, 13%)" />
+			<BoxLink a path={linkedin} img="/linkedin.svg" color="royalblue" />
+			<BoxLink a path={youtube} img="/youtube.svg" color="red" />
+			<BoxLink a path={instagram} img="/instagram.svg" color="magenta" />
+			<BoxLink a path={discord} img="/discord.svg" color="hsl(266, 45%, 40%)" />
+		</nav>
+	);
 }
