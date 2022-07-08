81d494bedfb41aac1670a6a25ecf766c33c15d44
Edwin Ortiz <edarcode@gmail.com>
2022-07-07 21:17:17 -0500

    refactor(src/data/projects.js)

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,17 +7,17 @@ export const projects = [
 		video: ""
 	},
 	{
-		name: "gusakafe",
-		text: "App en tiempo real, el cliente ocupa una mesa, crear un pedido y los chef atienden la orden",
-		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1657231239/apps/gusakafe_gfamju.png",
-		link: "pedding",
-		video: ""
-	},
-	{
 		name: "edar ecommerce",
 		text: "Pago con tarjeta (stripe). Filtros para los productos, detalle, Login (requiere verificarse), reset password, agg productos al carrito o sacarlos, ",
 		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1657240356/apps/edar_ecommerce_kz4jhm.png",
 		link: "https://edar-ecommerce.vercel.app/",
+		video: ""
+	},
+	{
+		name: "gusakafe",
+		text: "App en tiempo real, el cliente ocupa una mesa, crea un pedido y los chef atienden la orden",
+		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1657231239/apps/gusakafe_gfamju.png",
+		link: "pedding",
 		video: ""
 	}
 ];
