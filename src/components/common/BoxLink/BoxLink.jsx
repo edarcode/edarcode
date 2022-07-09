3802e2374e535ad3205620787432701238aaa5b4
Edwin Ortiz <edarcode@gmail.com>
2022-07-09 01:29:45 -0500

    feat(src/components/common/BoxLink/BoxLink.jsx)

--- src/components/common/BoxLink/BoxLink.jsx
+++ src/components/common/BoxLink/BoxLink.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import css from "./style.module.css";
 
 export default function BoxLink({ text, img, color, path, a, className }) {
 	const link = useRef(null);
+	const { pathname } = useLocation();
 	useEffect(() => {
 		if (color) link.current.style.color = color;
 	}, [color]);
+
 	if (!path) return null;
 	if (a)
 		return (
@@ -22,7 +24,14 @@ export default function BoxLink({ text, img, color, path, a, className }) {
 			</a>
 		);
 	return (
-		<Link to={path} className={`${css.box} ${className}`} ref={link}>
+		<Link
+			to={path}
+			className={`${css.box} ${className}`}
+			ref={link}
+			style={
+				(pathname === path && { backgroundColor: color || "var(--blue)" }) || {}
+			}
+		>
 			{img && <img className={css.box__img} src={img} alt={img} />}
 			{text && <span className={css.box__text}>{text}</span>}
 		</Link>
