950db31d5b63d7c9dba4e7e06c7384c28b0c3fa3
Edwin Ortiz <edarcode@gmail.com>
2022-07-09 01:43:12 -0500

    feat(src/components/common/TopicLink/TopicLink.jsx)

--- src/components/common/TopicLink/TopicLink.jsx
+++ src/components/common/TopicLink/TopicLink.jsx
@@ -1,9 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import css from "./style.module.css";
 
 export default function TopicLink({ path, name }) {
+	const { pathname } = useLocation();
 	return (
-		<Link to={path} className={css.topic}>
+		<Link
+			to={path}
+			className={css.topic}
+			style={(pathname === path && { backgroundColor: "var(--blue)" }) || {}}
+		>
 			{name}
 		</Link>
 	);
