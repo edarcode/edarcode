90c2b31762d84b3dd8dfecb18e326f7635a1730c
Edwin Ortiz <edarcode@gmail.com>
2022-07-11 19:23:06 -0500

    feat(src/components/container/SelectDomElements/SelectDomElements.jsx) seleccion de cajas del DOM done �

--- src/components/container/SelectDomElements/SelectDomElements.jsx
+++ src/components/container/SelectDomElements/SelectDomElements.jsx
@@ -1,21 +1,26 @@
+import css from "./style.module.css";
+
 export default function SelectDomElements() {
 	return (
-		<article>
+		<article className={css.article}>
 			<header>
-				<p>
+				<p className={css.article__quote}>
 					Si nos encontramos en nuestro código Javascript y queremos hacer
-					modificaciones en una etiqueta, lo primero que debemos hacer es
-					seleccionarla. Para ello, se suele identificar la etiqueta a través de
-					alguno de sus atributos más utilizados, el id o la clase.
+					modificaciones en una <strong>etiqueta</strong>, lo primero que
+					debemos hacer es <strong>seleccionarla</strong>. Para ello, se suele
+					identificar la etiqueta a través de alguno de sus atributos más
+					utilizados, usualmente el <strong>id</strong> o la{" "}
+					<strong>clase.</strong>
 				</p>
 			</header>
-			<section>
-				<h2>Métodos tradicionales 👵</h2>
+			<section className={css.article__traditional}>
+				<h2 className={css.article__h2}>Métodos tradicionales 👵</h2>
 				<p>
-					Estos metodos son vieja escuela 👴, está bien conocerlos, pero no te
-					centres mucho en ellos. De hecho hasta me da pereza explicarlos 😴.
+					Estos metodos son vieja escuela 👴, no va mal conocerlos, pero no
+					centres demasiado tu atención en ellos. De hecho, me da pereza
+					explicarlos 😴.
 				</p>
-				<table>
+				<table className={css.article__table}>
 					<thead>
 						<tr>
 							<th>Funciones de búsqueda</th>
@@ -32,38 +37,43 @@ export default function SelectDomElements() {
 						<tr>
 							<td>document.getElementsByClassName(class)</td>
 							<td>
-								Busca etiquetas con la clase especificada. Si no, devuelve [].
+								Busca etiquetas con la clase especificada. Si no, devuelve
+								&quot;[ ]&quot;
 							</td>
 						</tr>
 						<tr>
 							<td>document.getElementsByName(name)</td>
 							<td>
-								Busca etiquetas con el name especificado. Si no, devuelve [].
+								Busca etiquetas con el name especificado. Si no, devuelve
+								&quot;[ ]&quot;
 							</td>
 						</tr>
 						<tr>
 							<td>document.getElementsByTagName(tag)</td>
-							<td>Busca etiquetas del tipo tag. Si no, devuelve [].</td>
+							<td>
+								Busca etiquetas del tipo tag. Si no, devuelve &quot;[ ]&quot;
+							</td>
 						</tr>
 					</tbody>
 				</table>
-				<p>
-					👀 OJO al OJO 👀 Aunque hago referencia a que los metodos devuelven un
-					[], realmente los métodos de búsqueda generalmente devuelven un tipo
-					de dato <strong>HTMLCollection</strong> o <strong>NodeList</strong>,
-					que aunque actúan de forma muy similar a un [], no son arrays, y por
-					lo tanto pueden carecer de algunos métodos, como por ejemplo
-					.forEach().
+				<p className={css.article__alert}>
+					¡👀 OJO al OJO 👀! Aunque hago referencia a que los métodos devuelven
+					un
+					<strong>[ ]</strong>, realmente devuelven un tipo de dato{" "}
+					<strong>HTMLCollection</strong> o <strong>NodeList</strong>, aunque
+					actúan de forma muy similar a un <strong>[ ]</strong>, no son arrays,
+					y por lo tanto pueden carecer de algunos métodos, por ejemplo
+					.forEach() o .map().
 				</p>
 			</section>
-			<section>
-				<h1>Métodos modernos 😎</h1>
+			<section className={css.article__modern}>
+				<h1 className={css.article__title}>Métodos modernos 😎</h1>
 				<p>
-					Actualmente tenemos a nuestra disposición dos nuevos métodos de
-					búsqueda de elementos que son mucho más cómodos y prácticos si
-					conocemos los selectores <strong>CSS.</strong>
+					Actualmente tenemos a nuestra disposición 2 nuevos métodos de
+					selección de etiquetas que son mucho más cómodos y prácticos si
+					conocemos <strong>Selectores CSS 💅.</strong>
 				</p>
-				<table>
+				<table className={css.article__table}>
 					<thead>
 						<tr>
 							<th>Funciones de búsqueda</th>
@@ -72,49 +82,53 @@ export default function SelectDomElements() {
 					</thead>
 					<tbody>
 						<tr>
-							<td>document.querySelector(selector)</td>
 							<td>
-								Busca <strong>La primer etiqueta</strong> que coincide con el
-								selector especificado. Si no, devuelve null{" "}
+								<strong>document.querySelector(selector)</strong>
+							</td>
+							<td>
+								Busca <strong>La primer etiqueta que coincide</strong> con el
+								selector especificado. Si no, devuelve <strong>null</strong>
 							</td>
 						</tr>
 						<tr>
-							<td>document.querySelectorAll(selector)</td>
 							<td>
-								Busca <strong>Todas las etiqueta</strong> que coinciden con el
-								selector especificado. Si no, devuelve []
+								<strong>document.querySelectorAll(selector)</strong>
+							</td>
+							<td>
+								Busca <strong>Todas las etiquetas que coinciden</strong> con el
+								selector especificado. Si no, devuelve <strong>[ ]</strong>
 							</td>
 						</tr>
 					</tbody>
 				</table>
 				<p>
 					Con estos dos métodos podemos realizar todo lo que hacíamos con los
-					métodos tradicionales e incluso muchas más cosas (en menos code), son
-					muy flexibles y potentes gracias a los selectores CSS.
+					métodos tradicionales e incluso muchas más cosas (en{" "}
+					<strong>menos code</strong>), son muy <strong>flexibles</strong> y{" "}
+					<strong>potentes</strong> gracias a los selectores CSS.
 				</p>
-				<h2>document.querySelector(&quot;Selector&quot;)</h2>
+				<h3 className={css.article__h3}>
+					document.querySelector(&quot;Selector&quot;)
+				</h3>
 				<img src="/selectDom/query_selector.png" alt="querySelector" />
 				<p>
-					El método devuelve la <strong>Primera etiqueta</strong> que encuentra
-					que encaje con el <strong>Selector CSS</strong> indicado como
-					parámetro, si no, devuelte <strong>null</strong>. Y es realmente 🦾
-					potente 🦾, podemos pasarle el selector que nos apetezca por muy
-					complejo que sea.
+					El método devuelve la <strong>Primera etiqueta</strong> que encaje con
+					el <strong>Selector CSS</strong> indicado como parámetro, si no,
+					devuelte <strong>null</strong>. Y es realmente 🦾 potente 🦾, podemos
+					pasarle el selector que nos apetezca por muy avanzado que sea.
 				</p>
 				<img
 					src="/selectDom/result_query_selector.png"
 					alt="resultado del querySelector"
 				/>
-				<h2>document.querySelectorAll(&quot;Selector&quot;)</h2>
+				<h3 className={css.article__h3}>
+					document.querySelectorAll(&quot;Selector&quot;)
+				</h3>
 				<p>
 					Devuelve un <strong>NodeList</strong> (similar a un{" "}
 					<strong>Array</strong>) y contiene{" "}
 					<strong>todas las etiquetas que coinciden</strong> con el{" "}
 					<strong>Selector CSS</strong>:
-				</p>
-				<p>
-					El <strong>NodeList</strong> no deja usar metodos como map, forEach y
-					otros que si que podemos aplicar a los <strong>arrays</strong>.
 				</p>
 				<img src="/selectDom/query_selector_all.png" alt="querySelectorAll" />
 				<img
@@ -122,15 +136,19 @@ export default function SelectDomElements() {
 					alt="resultado del querySelectorAll"
 				/>
 				<p>
-					En JavaScript moderno, una de las formas de convertir un{" "}
-					<strong>NodeList</strong> en un <strong>Array</strong> es usando el
-					método <strong>Array.from()</strong>, este metodo es{" "}
-					<strong>Rechulo</strong> y hace mas cositas, dale un 👀 luego.
+					El <strong>NodeList</strong> NO dispone de métodos como map, forEach y
+					otros que si que podemos aplicar a los <strong>arrays</strong>.
 				</p>
-				<h2>Array.from()</h2>
 				<p>
-					El método Array.from() crea una nueva instancia de Array a partir de
-					un objeto iterable.
+					En JavaScript moderno, una de las formas de convertir un{" "}
+					<strong>NodeList</strong> a un <strong>Array</strong> es usando el
+					método <strong>Array.from()</strong>, este método es{" "}
+					<strong>Rechulo 😎</strong> y hace mas cositas, dale un 👀 luego.
+				</p>
+				<h4 className={css.article__h4}>Array.from()</h4>
+				<p>
+					El método <strong>Array.from()</strong> crea una nueva instancia de
+					Array a partir de un objeto iterable.
 				</p>
 				<img
 					src="/selectDom/query_selector_all_array_from.png"
@@ -144,8 +162,8 @@ export default function SelectDomElements() {
 					Al seleccionar una etiqueta y guardarla en una variable, podemos
 					realizar selecciones sobre esa variable en lugar de hacerla sobre{" "}
 					<strong>document</strong>. Así acotamos la selección por zonas, en
-					lugar de realizarlo siempre sobre <strong>document</strong>, que
-					buscará en todo el <strong>DOM</strong>.
+					lugar de hacerlo sobre <strong>document</strong>, que buscará en todo
+					el <strong>DOM 😛</strong>.
 				</p>
 				<img
 					src="/selectDom/delimit.png"
