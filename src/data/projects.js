abe85c6bd7f1a49d5673e9d565fc5405d9d35207
Edwin Ortiz <edarcode@gmail.com>
2022-07-15 06:13:46 -0500

    refactor(src/data/projects.js)

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,20 +1,20 @@
 export const projects = [
 	{
-		name: "player mp3",
+		name: "player mp3 🎵",
 		text: "Reproductor mp3 hecho con FULL js vanilla",
 		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1657800922/apps/mp3_vn9cks.png",
 		link: "https://player-mp3.vercel.app/",
 		video: ""
 	},
 	{
-		name: "rick and morty",
+		name: "rick and morty 🤪",
 		text: "Buscador de personajes, puede agg a favoritos o eliminar. Landing con sliders",
 		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1657240356/apps/rick_zehkc3.png",
 		link: "https://rick-amber.vercel.app/",
 		video: ""
 	},
 	{
-		name: "edar ecommerce",
+		name: "edar ecommerce 🤑",
 		text: "Pago con tarjeta (stripe). Filtros para los productos, detalle, Login (requiere verificarse), reset password, agg productos al carrito o sacarlos",
 		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1657240356/apps/edar_ecommerce_kz4jhm.png",
 		link: "https://edar-ecommerce.vercel.app/",
@@ -22,7 +22,7 @@ export const projects = [
 	},
 
 	{
-		name: "gusakafe",
+		name: "gusakafe (en desarrollo) ☕",
 		text: "App en tiempo real, el cliente ocupa una mesa, crea un pedido y los chef atienden la orden",
 		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1657231239/apps/gusakafe_gfamju.png",
 		link: "https://gusakafe.vercel.app/",
