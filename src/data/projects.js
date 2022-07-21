975ddfc8b8df2226505297888dda636cd6dbf93c
edarcode <edarcode@gmail.com>
2022-07-21 00:36:03 -0500

    feat(src/data/projects.js) cambio de img a .webp

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,21 +2,21 @@ export const projects = [
 	{
 		name: "player mp3 🎵",
 		text: "Reproductor mp3 hecho con FULL js vanilla",
-		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1657800922/apps/mp3_vn9cks.png",
+		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1658381576/apps/mp3_vn9cks_cy6eye.webp",
 		link: "https://player-mp3.vercel.app/",
 		video: ""
 	},
 	{
 		name: "rick and morty 🤪",
 		text: "Buscador de personajes, puede agg a favoritos o eliminar. Landing con sliders",
-		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1657240356/apps/rick_zehkc3.png",
+		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1658381576/apps/rick_zehkc3_yuvjjn.webp",
 		link: "https://rick-amber.vercel.app/",
 		video: ""
 	},
 	{
 		name: "edar ecommerce 🤑",
 		text: "Pago con tarjeta (stripe). Filtros para los productos, detalle, Login (requiere verificarse), reset password, agg productos al carrito o sacarlos",
-		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1657240356/apps/edar_ecommerce_kz4jhm.png",
+		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1658381576/apps/edar_ecommerce_kz4jhm_f3pdxd.webp",
 		link: "https://edar-ecommerce.vercel.app/store",
 		video: ""
 	},
@@ -24,7 +24,7 @@ export const projects = [
 	{
 		name: "gusakafe (en desarrollo) ☕",
 		text: "App en tiempo real, el cliente ocupa una mesa, crea un pedido y los chef atienden la orden",
-		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1657231239/apps/gusakafe_gfamju.png",
+		img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1658381576/apps/gusakafe_gfamju_ctsnd5.webp",
 		link: "https://gusakafe.vercel.app/",
 		video: ""
 	}
