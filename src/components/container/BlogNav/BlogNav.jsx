3854d13636d2d9f7721ece9d44d434baefaf57cf
edarcode <edarcode@gmail.com>
2022-07-24 02:38:08 -0500

    feat(src/components/container/BlogNav/style.module.css)

--- src/components/container/BlogNav/BlogNav.jsx
+++ src/components/container/BlogNav/BlogNav.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { CgCloseO } from "react-icons/cg";
 import { FaBars } from "react-icons/fa";
 import { home, projects } from "../../../constants/pathRoute";
 import { IsCollapseAside } from "../../../contexts/IsCollapseAside";
@@ -6,15 +7,20 @@ import BoxLink from "../../common/BoxLink/BoxLink";
 import css from "./style.module.css";
 
 export default function BlogNav() {
-	const { setIsCollapseMenu } = useContext(IsCollapseAside);
+	const { isCollapseMenu, setIsCollapseMenu } = useContext(IsCollapseAside);
+	const handleOnClickFabar = e => {
+		setIsCollapseMenu(state => !state);
+	};
 	return (
 		<nav className={css.blog}>
 			<BoxLink path={home} text="Inicio" className={css.blog__link} />
 			<BoxLink path={projects} text="Proyectos" className={css.blog__link} />
-			<FaBars
-				className={css.blog__fabars}
-				onClick={() => setIsCollapseMenu(state => !state)}
-			/>
+			{!isCollapseMenu && (
+				<FaBars className={css.blog__fabars} onClick={handleOnClickFabar} />
+			)}
+			{isCollapseMenu && (
+				<CgCloseO className={css.blog__fabars} onClick={handleOnClickFabar} />
+			)}
 		</nav>
 	);
 }
