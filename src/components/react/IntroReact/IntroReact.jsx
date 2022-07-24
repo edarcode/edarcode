5cdea52cdf0461571fd31a63bc17c60a777b8721
edarcode <edarcode@gmail.com>
2022-07-24 18:04:24 -0500

    feat(src/components/react/Requisites/Requisites.jsx)

--- src/components/react/IntroReact/IntroReact.jsx
+++ src/components/react/IntroReact/IntroReact.jsx
@@ -4,7 +4,6 @@ import css from "./style.module.css";
 export default function IntroReact() {
 	return (
 		<div className={css.intro}>
-			Intro React
 			<Outlet />
 		</div>
 	);
--- src/components/react/Requisites/Requisites.jsx
+++ src/components/react/Requisites/Requisites.jsx
@@ -1,3 +1,26 @@
 export default function Requisites() {
-	return <div>Requisites</div>;
+	return (
+		<article className="article">
+			<h1>Requisitos 😱</h1>
+			<p>
+				Lo ideal es que domine cada uno de estos puntos, NO debe ser un experto.
+			</p>
+			<ul>
+				<li>Resolver katas(ejercicios) de js 🧐.</li>
+				<li>Maquetar una web con HTML y estilarla con CSS 🥸.</li>
+				<li>
+					Manejo del package.json e instalación de dependencias con npm para
+					c/proyecto 🥱.
+				</li>
+				<li>Ser gomoso, activo y con muchas ganas de aprender 💯.</li>
+			</ul>
+			<p className="quote">
+				Acepto que NO soy tan bueno con los katas, pero igual me lancé a
+				aprender React, estaba aburrido de copiar y pegar la misma estructura en
+				HTML al maquetar una web 💩. Trabajar con componentes es super comodo,
+				vas a escribir menos code, facil de leer y escalar.
+			</p>
+			<h2>&quot;Larga vida al 👑&quot;</h2>
+		</article>
+	);
 }
