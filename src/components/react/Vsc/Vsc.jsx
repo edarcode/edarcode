6f431126189c55bd0b5362b2ddf90ae1caeb470a
edarcode <edarcode@gmail.com>
2022-07-25 00:36:51 -0500

    feat(src/components/react/Vsc/Vsc.jsx) done

--- src/components/react/Vsc/Vsc.jsx
+++ src/components/react/Vsc/Vsc.jsx
@@ -1,7 +1,7 @@
 export default function Vsc() {
 	return (
-		<article>
-			<section>
+		<article className="article">
+			<section className="section">
 				<h1>Visual Studio Code</h1>
 				<p>
 					Este editor es el mas famoso y utilizado para provectos React, es
@@ -16,7 +16,7 @@ export default function Vsc() {
 					Sitio oficial para descargar VSC
 				</a>
 			</section>
-			<section>
+			<section className="section">
 				<h2>Extensiones</h2>
 				<p>
 					En este apartado podremos buscar extensiones que deseamos instalar. Es
@@ -30,12 +30,12 @@ export default function Vsc() {
 					src="/react/extensiones.webp"
 					alt="apartado para instalar extensiones"
 				/>
-				<section>
+				<section className="section">
 					<h3>Tema de Color</h3>
 					<p>
 						A mi me encanta usar gruvbox, lo conocí una vez que estaba
 						intentando pasarme a nvim y me enamoré del tema. En VSC vas a
-						encontrar muchos resultados, yo recomiendo Gruvbox Theme:
+						encontrar muchos resultados, yo recomiendo Gruvbox Theme
 					</p>
 					<img src="/react/gruvbox.webp" alt="extension gruvbox" />
 					<span>En particular la config de Dark Medium</span>
@@ -43,7 +43,58 @@ export default function Vsc() {
 					<span>Un ejemplo de como se ve</span>
 					<img src="/react/example_gruvbox.webp" alt="ejemplo gruvbox" />
 				</section>
+				<section className="section">
+					<h3>Iconos para carpetas y archivos</h3>
+					<p>
+						Es mucho mas sencillo y agradable a la vista ver un icono que haga
+						referencia a cada tipo de fichero o carpeta, Material Icons Theme
+						nos ayudará con esto.
+					</p>
+					<img
+						src="/react/material_icons.webp"
+						alt="extension material icons themes"
+					/>
+					<p>
+						Por defecto está configurada con los icons de Angular, pero vamos a{" "}
+						<span className="special_word">Administrar</span> {"=>"}{" "}
+						<span className="special_word">Configuración</span> {"=>"}{" "}
+						<span className="special_word">Extensiones</span> {"=>"}{" "}
+						<span className="special_word">Material Icons</span> {"=>"}{" "}
+						<span className="special_word">Active Icon Pack</span>, despliegas y
+						elije React. Si eres ciego como yo, el botón de Administrar está en
+						al parte inferior izquierda del editor.
+					</p>
+
+					<img src="/react/admin_vsc.webp" alt="admin vsc" />
+					<p>
+						Mas rápido sería usando directamente el atajo{" "}
+						<span className="special_word">Ctrl + ,</span> que nos lleva directo
+						a configuración
+					</p>
+					<img
+						src="/react/config_material_icons.webp"
+						alt="config material icons"
+					/>
+				</section>
+				<section className="section">
+					<h3>Auto Rename Tag</h3>
+					<p>
+						Cuando cambies el nombre de una etiqueta HTML/XML, cambiará
+						automaticamente su pareja.
+					</p>
+					<img src="/react/rename_tag.webp" alt="rename tag" />
+				</section>
 			</section>
+			<p>
+				Hasta aquí por ahora, ESLint y Prettier tendrán su propio apartado,
+				igualmente habrá un apartado de como crear Snippets personalizados.
+			</p>
+			<footer className="quote">
+				Quedó re-chulo nuestro editor ¿verdad? 😎. Seguramente estas extensiones
+				de VSC te hacen mas feliz y por ende una mejor persona 👻, incluso se
+				especula por ahí que te sube las defensas, aleja los malos espíritus y
+				hasta pregonan buena fortuna 😆.
+			</footer>
 		</article>
 	);
 }
