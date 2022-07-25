bb84f3180844ccf9cbdda8e1424b95cc8e40e5ab
edarcode <edarcode@gmail.com>
2022-07-24 19:37:47 -0500

    feat(src/components/react/Chrome/Chrome.jsx) done

--- src/components/react/Chrome/Chrome.jsx
+++ src/components/react/Chrome/Chrome.jsx
@@ -1,3 +1,14 @@
 export default function Chrome() {
-	return <div>Chrome</div>;
+	return (
+		<article className="article">
+			<h1>Chrome y React dev tools</h1>
+			<p>
+				Es recomendable utilizar google chrome para seguir el temario y además
+				instalar la extensión React Developer Tools. Esto permite ver que webs
+				están utilizando React y además nos da herramientas muy útiles para
+				desarrollo.
+			</p>
+			<img src="/public/react/react_dev_tools.webp" alt="react dev tools" />
+		</article>
+	);
 }
