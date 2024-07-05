240604268b156a63f04303202ba7ed1e3392c2e7
Edwin Ortiz <edarcode@gmail.com>
2024-07-04 19:40:34 -0500

    avance about me

--- src/views/AboutMe/AboutMe.tsx
+++ src/views/AboutMe/AboutMe.tsx
@@ -1,17 +1,31 @@
+import Css from "../../components/svgs/Css";
 import Html from "../../components/svgs/Html";
+import Js from "../../components/svgs/Js";
+import React from "../../components/svgs/React";
 import { EDARCODE } from "../../constants/edarcode";
 import css from "./css.module.css";
 
 export default function AboutMe() {
 	return (
 		<div className={css.about}>
-			<img className={css.img} src={EDARCODE.img} alt="edarcode" />
-			<p>
-				Siempre atento a aprender de los demás y a compartir conocimiento. Creo
-				en el trabajo duro y el compromiso.
-			</p>
-			<h2>Habilidades</h2>
-			<Html />
+			<article>
+				<h1>Edwin Ortiz</h1>
+				<h2>Full Stack Developer</h2>
+				<img className={css.img} src={EDARCODE.img} alt="edarcode" />
+				<p>
+					Siempre atento a aprender de los demás y a compartir conocimiento.
+					Creo en el trabajo duro y el compromiso.
+				</p>
+			</article>
+			<article>
+				<h2>Habilidades</h2>
+				<div>
+					<Html />
+					<Css />
+					<Js />
+					<React />
+				</div>
+			</article>
 		</div>
 	);
 }
