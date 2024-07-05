a3bdf03b2328adf5b717976db398b0f2481e2c22
Edwin Ortiz <edarcode@gmail.com>
2024-07-05 01:07:51 -0500

    avance about me

--- src/views/AboutMe/AboutMe.tsx
+++ src/views/AboutMe/AboutMe.tsx
@@ -1,14 +1,19 @@
 import Css from "../../components/svgs/Css";
+import Express from "../../components/svgs/Express";
 import Html from "../../components/svgs/Html";
 import Js from "../../components/svgs/Js";
+import Nodejs from "../../components/svgs/Nodejs";
+import Postgre from "../../components/svgs/Postgre";
+import Prisma from "../../components/svgs/Prisma";
 import React from "../../components/svgs/React";
+import TypeScript from "../../components/svgs/TypeScript";
 import { EDARCODE } from "../../constants/edarcode";
 import css from "./css.module.css";
 
 export default function AboutMe() {
 	return (
 		<div className={css.about}>
-			<article>
+			<article className={css.me}>
 				<h1>Edwin Ortiz</h1>
 				<h2>Full Stack Developer</h2>
 				<img className={css.img} src={EDARCODE.img} alt="edarcode" />
@@ -17,13 +22,18 @@ export default function AboutMe() {
 					en el trabajo duro y el compromiso.
 				</p>
 			</article>
-			<article>
+			<article className={css.skills}>
 				<h2>Habilidades</h2>
-				<div>
+				<div className={css.logos}>
 					<Html />
 					<Css />
 					<Js />
 					<React />
+					<Nodejs />
+					<Express />
+					<TypeScript />
+					<Prisma />
+					<Postgre />
 				</div>
 			</article>
 		</div>
