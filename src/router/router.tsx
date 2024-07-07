f34f2b742cd3b894590af1038ecbe6e24d2131f9
Edwin Ortiz <edarcode@gmail.com>
2024-07-07 10:25:20 -0500

    refactor

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,7 +4,6 @@ import { APP_PATH } from "../constants/appPaths";
 import AboutMe from "../views/AboutMe/AboutMe";
 import Education from "../views/Education/Education";
 import ErrPage from "../views/ErrPage/ErrPage";
-import Projects from "../views/Projects/Projects";
 import Utilities from "../views/Utilities/Utilities";
 
 export const router = createBrowserRouter([
@@ -14,7 +13,6 @@ export const router = createBrowserRouter([
 		errorElement: <ErrPage />,
 		children: [
 			{ path: APP_PATH.index, element: <AboutMe /> },
-			{ path: APP_PATH.projects, element: <Projects /> },
 			{ path: APP_PATH.utilities, element: <Utilities /> },
 			{ path: APP_PATH.education, element: <Education /> }
 		]
