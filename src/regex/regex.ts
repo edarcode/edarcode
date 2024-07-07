d530a6d755ca738ed53915017e7c4d6f1d85489c
Edwin Ortiz <edarcode@gmail.com>
2024-07-07 11:27:39 -0500

    borrando archivos innecesarios

--- src/regex/regex.ts
+++
@@ -1,5 +0,0 @@
-export const REGEX = {
-	onlyLettersAndNums: /^[a-z0-9]+$/, // ^ inicio $ fin + puede repetir
-	startWithNum: /^[0-9]/,
-	onlyLetters: /^[a-záéíóú\s]+$/i // i mayus o minus, \s acepta espacios
-};
