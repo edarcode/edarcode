be25222f0c6503e38bebf35f2c365e10c4221746
Edwin Ortiz <edarcode@gmail.com>
2024-07-07 20:29:34 -0500

    cambio foto de perfil

--- src/constants/edarcode.ts
+++ src/constants/edarcode.ts
@@ -1,6 +1,6 @@
 export const EDARCODE = {
 	name: "edwin ortiz",
-	img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1720284695/edarcode/1-edarcode.avif",
+	img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1720401602/edarcode/edarcode.avif",
 	Skills: [
 		{
 			name: "HTML",
