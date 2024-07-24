7ac2af2bc6793854608894dbc61af6d909b95f9e
Edwin Ortiz <edarcode@gmail.com>
2024-07-23 19:41:49 -0500

    agg tamaño a los svg

--- src/components/inputs/InputText/Input/State/icons/Err.tsx
+++ src/components/inputs/InputText/Input/State/icons/Err.tsx
@@ -7,6 +7,8 @@ export default function Err(props: Props) {
 			stroke="currentColor"
 			viewBox="0 0 24 24"
 			xmlns="http://www.w3.org/2000/svg"
+			width="64"
+			height="64"
 			{...props}
 		>
 			<path
