a8588f77ec769a48c4d8498ac9fedba24030b1c1
Edwin Ortiz <edarcode@gmail.com>
2024-07-25 15:49:50 -0500

    centrando login

--- src/App/pages/Login/Login.tsx
+++ src/App/pages/Login/Login.tsx
@@ -1,15 +1,16 @@
 import Btn from "../../../components/buttons/Btn/Btn";
+import InputPassword from "../../../components/inputs/InputPassword/InputPassword";
 import InputText from "../../../components/inputs/InputText/InputText";
 import Hyperlink from "../../../components/links/Hyperlink/Hyperlink";
 import css from "./css.module.css";
 
 export default function Login() {
 	return (
-		<article className={css.login}>
+		<form className={css.login}>
 			<InputText placeholder="Correo" />
-
+			<InputPassword />
 			<Hyperlink href="#">Ir a registro</Hyperlink>
 			<Btn disabled>Login</Btn>
-		</article>
+		</form>
 	);
 }
