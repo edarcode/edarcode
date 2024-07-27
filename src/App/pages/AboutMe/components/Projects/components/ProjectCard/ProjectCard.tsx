6a0f6db27ed79e10476ce004566cc8b2ec065bf3
Edwin Ortiz <edarcode@gmail.com>
2024-07-27 10:51:11 -0500

    refactor

--- src/App/pages/AboutMe/components/Projects/components/ProjectCard/ProjectCard.tsx
+++ src/App/pages/AboutMe/components/Projects/components/ProjectCard/ProjectCard.tsx
@@ -2,7 +2,7 @@ import css from "./css.module.css";
 import { Props } from "./types";
 
 export default function ProjectCard({ project }: Props) {
-	const { title, description, tools, deploy } = project;
+	const { title, description, tools } = project;
 	return (
 		<article className={css.project}>
 			<h4 className={css.title}>{title}</h4>
