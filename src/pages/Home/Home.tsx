6ba71c4441e2acd52414ffa8488e38f027bab6bd
Edwin Ortiz <edarcode@gmail.com>
2024-09-28 17:15:56 -0500

    agg link de descarga cv

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Download from "../../components/links/Download/Download";
 import Banner from "./Banner/Banner";
 import css from "./css.module.css";
 import Education from "./Education/Education";
@@ -6,13 +7,16 @@ import Projects from "./Projects/Projects";
 import Skills from "./Skills/Skills";
 
 export default function Home() {
-  return (
-    <div className={css.home}>
-      <Banner />
-      <Experiences />
-      <Projects />
-      <Skills />
-      <Education />
-    </div>
-  );
+	return (
+		<div className={css.home}>
+			<Banner />
+			<Experiences />
+			<Projects />
+			<Skills />
+			<Education />
+			<Download className={css.cv} href="/cv.pdf" download="cv_edarcode">
+				Currículo
+			</Download>
+		</div>
+	);
 }
