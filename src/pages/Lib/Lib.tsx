e3aea2213635eb1d9515179ff48f6175f7412e85
Edwin Ortiz <edarcode@gmail.com>
2024-10-01 07:16:39 -0500

    Btn funcional

--- src/pages/Lib/Lib.tsx
+++ src/pages/Lib/Lib.tsx
@@ -1,5 +1,10 @@
+import Btn from "../../components/buttons/Btn/Btn";
 import css from "./css.module.css";
 
 export default function Lib() {
-	return <div className={css.lib}>Lib</div>;
+	return (
+		<div className={css.lib}>
+			<Btn>Enviar</Btn>
+		</div>
+	);
 }
