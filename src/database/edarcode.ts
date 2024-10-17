7435077601c6ad82d03fcb826310113c96b25df7
Edwin Ortiz <edarcode@gmail.com>
2024-10-17 15:46:28 -0500

    agg nuevo proyecto

--- src/database/edarcode.ts
+++ src/database/edarcode.ts
@@ -76,18 +76,20 @@ export const EDARCODE = {
 	],
 	projects: [
 		{
-			title: "Notaku",
-			dev: "Front-end",
-			deploy: "https://notaku.vercel.app/",
+			title: "Chat",
+			dev: "Full-stack",
+			deploy: "https://chat-beta-umber.vercel.app",
 			description:
-				"Encontrarás las últimas noticias, novedades del mundo del anime, junto con una herramienta de búsqueda que permite filtrar animes por título, género, popularidad y mucho más. Los datos son consultados en la API de kitsu.",
-			tools: "Vite, React, TS, TanStack Query, React Hook Form",
+				"Chat online. Envía mensajes, ya sea con tus amigos, familia o compañeros de trabajo, todo desde una sola aplicación. Los datos persisten en una base de datos alojada en Turso.",
+			tools:
+				"Vite, React, TS, TanStack Query, React Hook Form, Node.js, Express, DrizzleORM, SQLite",
 			imgs: {
 				mobile:
-					"https://res.cloudinary.com/dvmsllkkg/image/upload/v1728562121/edarcode/notaku_mobile.png",
-				pc: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1728562121/edarcode/notaku_desktop.png"
+					"https://res.cloudinary.com/dvmsllkkg/image/upload/v1729197397/edarcode/tk3oadwm7k3ctyc9tey8.png",
+				pc: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1729197397/edarcode/ui4gal77ioq3cmrvf7bw.png"
 			}
 		},
+
 		{
 			title: "Contacts",
 			dev: "Full-stack",
@@ -100,6 +102,19 @@ export const EDARCODE = {
 				mobile:
 					"https://res.cloudinary.com/dvmsllkkg/image/upload/v1728562121/edarcode/contacts_mobile.png",
 				pc: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1728562121/edarcode/contacts_desktop.png"
+			}
+		},
+		{
+			title: "Notaku",
+			dev: "Front-end",
+			deploy: "https://notaku.vercel.app/",
+			description:
+				"Encontrarás las últimas noticias, novedades del mundo del anime, junto con una herramienta de búsqueda que permite filtrar animes por título, género, popularidad y mucho más. Los datos son consultados en la API de kitsu.",
+			tools: "Vite, React, TS, TanStack Query, React Hook Form",
+			imgs: {
+				mobile:
+					"https://res.cloudinary.com/dvmsllkkg/image/upload/v1728562121/edarcode/notaku_mobile.png",
+				pc: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1728562121/edarcode/notaku_desktop.png"
 			}
 		}
 	]
