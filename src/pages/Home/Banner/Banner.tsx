f437b9915763a5517d5f6db7a8bae3f905b1567a
Edwin Ortiz <edarcode@gmail.com>
2024-10-23 20:43:06 -0500

    refactor

--- src/pages/Home/Banner/Banner.tsx
+++ src/pages/Home/Banner/Banner.tsx
@@ -1,17 +1,18 @@
 import css from "./css.module.css";
 
 export default function Banner() {
-  return (
-    <article className={css.banner}>
-      <div className={css.description}>
-        <h1>Edwin Ortiz</h1>
-        <h2>Full Stack Developer</h2>
-        <p>
-          Siempre dispuesto a aprender y compartir conocimientos. Creo en el
-          trabajo duro y el compromiso.
-        </p>
-      </div>
-      <img className={css.photo} src="/edarcode.webp" alt="edarcode" />
-    </article>
-  );
+	return (
+		<article className={css.banner}>
+			<div className={css.description}>
+				<h1>Edwin Ortiz</h1>
+				<h2>Full Stack Developer</h2>
+				<p>
+					Creo en el trabajo duro y el compromiso. Siempre dispuesto a compartir
+					conocimientos, aprender continuamente y crecer profesional y
+					personalmente.
+				</p>
+			</div>
+			<img className={css.photo} src="/edarcode.webp" alt="edarcode" />
+		</article>
+	);
 }
