d62d903d45628f2b60b0d579ac1c5d784082d893
Edwin Ortiz <edarcode@gmail.com>
2024-10-31 19:54:40 -0500

    pasando de react a astro

---
+++ src/pages/router.ts
@@ -0,0 +1,14 @@
+export const ROUTER = {
+  index: {
+    href: "/",
+    display: "Inicio",
+  },
+  look: {
+    href: "/look/",
+    display: "Componentes",
+  },
+  utils: {
+    href: "/utils/",
+    display: "Utilidades",
+  },
+};
