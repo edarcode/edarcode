c93069f1e3a13d4f8536e36f3b0b58c143945b9b
Edwin Ortiz <edarcode@gmail.com>
2024-11-27 10:54:15 -0500

    mejorando seo

---
+++ src/layouts/Layout/seo.ts
@@ -0,0 +1,7 @@
+export const SEO = {
+  title: "Portafolio edarcode",
+  description:
+    "Soy un profesional dedicado al desarrollo de sitios web, enfocado en la calidad y el compromiso con cada proyecto.",
+  author: "edarcode",
+  img: "https://res.cloudinary.com/dvmsllkkg/image/upload/v1727950041/edarcode/edar.jpg",
+};
